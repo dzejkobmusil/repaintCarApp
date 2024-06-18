6cd5c110fce8e09b44646a90c4778ce407ad0664
Jakub Musil <it-jakubmusil@stredniskola.net>
2024-06-18 09:49:58 +0200

    Přidání funkce zvětšení kol

--- src/main/java/com/stredniskola/smistik/HelloController.java
+++ src/main/java/com/stredniskola/smistik/HelloController.java
@@ -1,19 +1,45 @@
 package com.stredniskola.smistik;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Polygon auto;
     @FXML
     private ColorPicker barva;
+    @FXML
+            private Circle zadni;
+    @FXML
+            private Circle predni;
+    @FXML
+            private Slider slider;
+
 
     boolean wracket = false;
     public void zmenBarvu(ActionEvent event) {
         auto.setFill(barva.getValue());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                predni.setRadius((Double) newValue);
+                zadni.setRadius((Double) newValue);
+            }
+        });
     }
 }
